ebbb337d075bd124e679b49cf31c47365e646eef
bluestar92011 <bluestar92011@gmail.com>
2018-08-07 18:59:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 def fizzbuzz(x)
- if x % 3 == 0 
-   puts "fizz"
-elsif x % 5 == 0 
-puts "buzz"  
-  
+  if x % 3 == 0 
+    puts "fizz"
+  elsif x % 5 == 0 
+    puts "buzz" 
+  elsif x % 15 == 0 
+    puts "fizzbuzz"
+  else
+    puts "nil"
   end
 end 
 
-fizzbuzz(10)
+fizzbuzz(5)
